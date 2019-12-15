dbeb654093fbb77256882b090c2df2b6eb85d2c3
Tristan Timblin <tatimblin@gmail.com>
2019-12-15 10:14:05 -0500

    edit bio

--- src/components/sections/ProfessionalInfo.vue
+++ src/components/sections/ProfessionalInfo.vue
@@ -24,9 +24,9 @@ export default {
         {
           value: `
             A software engineer focused on the human computer interaction between people, and the applications
-            they use day to day. I believe in building smaller solutions to service the same user needs faster and cheaper.
-            At Yext I help build and maintain the applications, that generate our pages. Together our service hosts
-            over a million static pages averaging +1.5 billion views a year, chances are you've alredy visited one.`,
+            they use day to day. At Yext I help build and maintain the applications, that generate our pages.
+            Together our service hosts over a million static pages averaging +1.5 billion views a year, chances
+            are you've alredy visited one.`,
         },
       ],
     }
